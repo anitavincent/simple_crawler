ca0e4f2df88cb7378714c20b8d203ad88a245598
anitavincent <paes.anita@gmail.com>
2018-05-14 21:14:22 -0300

    Add page class

---
+++ productpage.py
@@ -0,0 +1,39 @@
+import requests
+from bs4 import BeautifulSoup
+import re
+
+
+class ProductPage:
+
+    target_regex = ".*/p"
+
+    def __init__(self, page_url, base_url):
+        self.page_url = page_url
+        self.base_url = base_url
+        self.page_content = requests.get(page_url)
+        self.soup = BeautifulSoup(self.page_content.content, 'html.parser')
+
+    def parse(self):
+        links = self.get_links()
+        content = ""
+        if self.is_scrapping_target():
+            content = self.get_content()
+        return [links, self.soup]
+
+    def get_links(self):
+        links_list = []
+        a_tags = self.soup.find_all('a', href=True)
+        for tag_content in a_tags:
+            links_list.append(tag_content['href'])
+
+        return links_list
+
+    def get_content(self):
+        tags = self.soup.find_all("div", class_="productName")
+        return tags[0].text
+
+    def is_scrapping_target(self):
+        regex = "^{}{}$".format(self.base_url, self.target_regex)
+        if re.match(regex, self.page_url):
+            return True
+        return False
