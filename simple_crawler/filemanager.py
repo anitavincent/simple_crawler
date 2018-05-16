4e511050e6aaf10abd07aae5b9f4b7b685151e1c
anitavincent <paes.anita@gmail.com>
2018-05-15 22:43:06 -0300

    Implement FileManager

--- simple_crawler/filemanager.py
+++ simple_crawler/filemanager.py
@@ -1,23 +1,91 @@
+import csv
+import os.path
+import ast
+
+
 class FileManager:
 
     def __init__(self, found_urls_file, result_file):
         self.found_urls_file = found_urls_file
         self.result_file = result_file
+        self.data_directory = "../data/"
+
+    def is_empty(self, filename):
+        return os.stat(filename).st_size == 0
+
+    def get_full_path(self, file_name):
+
+        relative_path = "{}{}".format(self.data_directory, file_name)
+        current_path = os.path.dirname(__file__)
+        path = os.path.join(current_path, relative_path)
+
+        return path
 
     def get_urls_dict(self):
-        pass
 
-    def get_product_names_dict(self):
-        pass
+        return_dict = {}
+        file_location = self.get_full_path(self.found_urls_file)
 
-    def add_to_results(url, product_name, title):
-        pass
+        with open(file_location, 'r') as csvfile:
+            reader = csv.DictReader(csvfile)
+            for row in reader:
+                url = row["url"]
+                is_parsed = ast.literal_eval(row["is_parsed"])
+                return_dict[url] = is_parsed
 
-    def add_to_found_urls(url):
-        pass
+        return return_dict
+
+    def get_product_names_set(self):
+
+        return_set = set()
+        file_location = self.get_full_path(self.result_file)
+
+        with open(file_location, 'r') as csvfile:
+            reader = csv.DictReader(csvfile)
+            for row in reader:
+                name = row["product_name"]
+                return_set.add(name)
+
+        return return_set
+
+    def add_to_results(self, url, product_name, title):
+
+        file_location = self.get_full_path(self.result_file)
+
+        with open(file_location, 'a', newline='') as csvfile:
+
+            fieldnames = ['url', 'product_name', 'title']
+            writer = csv.DictWriter(csvfile, fieldnames=fieldnames)
+
+            if self.is_empty(file_location):
+                writer.writeheader()
+
+            writer.writerow(
+                {'url': url, 'product_name': product_name, 'title': title})
+
+    def add_to_found_urls(self, url):
+
+        file_location = self.get_full_path(self.found_urls_file)
+
+        with open(file_location, 'a', newline='') as csvfile:
+            fieldnames = ['url', 'is_parsed']
+            writer = csv.DictWriter(csvfile, fieldnames=fieldnames)
+
+            if self.is_empty(file_location):
+                writer.writeheader()
+
+            writer.writerow({'url': url, 'is_parsed': False})
 
     def clear_result(self):
-        pass
+
+        filename = self.get_full_path(self.result_file)
+
+        f = open(filename, "w+")
+        f.close()
 
     def clear_found_urls(self):
-        pass
+
+        filename = self.get_full_path(self.found_urls_file)
+
+        f = open(filename, "w+")
+        f.close()
--- tests/test_FileManager.py
+++ tests/test_FileManager.py
@@ -9,19 +9,31 @@ class FileManagerTest(TestCase):
 
     def test_change_products_result(self):
         self.manager.clear_result()
-        products = self.manager.get_product_names_dict()
-        self.assertEqual({}, products)
+        products = self.manager.get_product_names_set()
+        self.assertEqual(set(), products)
 
         self.manager.add_to_results("epoca.com", "Blush", "Blush - Epoca")
+
         self.manager.add_to_results("epoca.com", "Batom", "Batom")
-        products = self.manager.get_product_names_dict()
-        self.assertEqual({"Blush": True, "Batom": True}, products)
+        products = self.manager.get_product_names_set()
+        self.assertEqual({"Blush", "Batom"}, products)
 
         self.manager.clear_result()
-        products = self.manager.get_products_names_dict()
-        self.assertEqual({}, products)
+        products = self.manager.get_product_names_set()
+        self.assertEqual(set(), products)
 
     def test_change_found_urls(self):
+
+        self.manager.clear_found_urls()
+        urls = self.manager.get_urls_dict()
+        self.assertEqual({}, urls)
+
+        self.manager.add_to_found_urls("epoca.com")
+        self.manager.add_to_found_urls("google.com")
+
+        products = self.manager.get_urls_dict()
+        self.assertEqual(
+            {"epoca.com": False, "google.com": False}, products)
 
         self.manager.clear_found_urls()
         urls = self.manager.get_urls_dict()
