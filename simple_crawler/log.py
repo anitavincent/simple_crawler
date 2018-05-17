32e8d659defc9f1c7870c6a4e2238bb3a64f3d35
anitavincent <paes.anita@gmail.com>
2018-05-17 01:28:50 -0300

    Improve log class

--- simple_crawler/log.py
+++ simple_crawler/log.py
@@ -4,15 +4,15 @@ class Log:
         self.verbose = verbose
 
     def add_error(self, error_message):
-        print(error_message)
+        print("[Warning] {}".format(error_message))
 
-    def add_url_removal(self, reason="", url):
-        print("Removing {} from queue. {}".format(url, reason))
+    def add_url_removal(self, url, reason=""):
+        print("[Warning] Removing {} from queue. {}".format(url, reason))
 
     def add_pages_processed(self, number_of_pages, number_of_products):
-        if verbose:
-            print("{} pages processed, {} product pages found".format(
+        if self.verbose:
+            print("[Log] {} pages processed, {} product pages found".format(
                 number_of_pages, number_of_products))
 
     def add_message(self, message):
-        print(message)
+        print("[Log] {}".format(message))
